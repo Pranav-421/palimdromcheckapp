0f63b22bac7226dd99766b82a29105f52600c868
Pranav-421 <pg5975@srmist.edu.in>
2026-02-18 15:34:37 +0530

    Pranav Added UC2-HardCodePalidrom

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,9 +1,10 @@
-//version 1.0
-//author Gojo
-//use case 1: Welcome page
+//version 2.0
+//Author Gojo
+//use case 2:
 
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         System.out.println("Welcome to Palindrome Checker App Management System");
+        System. out. println("Modify this Logic to usecase 2");
     }
 }
